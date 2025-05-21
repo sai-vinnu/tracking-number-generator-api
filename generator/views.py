777e14bba7e5b192c6f191c975e25d2df1656306
Sai Vineeth Vallasani <svallasani@Vineeth-MacBook-Pro.local>
2025-05-21 11:40:54 +0530

    Added redis dymanically

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,7 +8,10 @@ import datetime
 import re
 from dateutil import parser
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+# redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+redis_url = os.getenv('REDIS_URL')
+redis_client = redis.from_url(redis_url, ssl=True)
 
 # Regex pattern to validate final tracking number
 TRACKING_REGEX = re.compile(r'^[A-Z0-9]{1,16}$')
